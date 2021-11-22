8ba7595c08ad8e1aad50a4379da00b6337a8150d
JavaidAhmadKhan <JavaidAhmadKhan@users.noreply.github.com>
2021-11-22 12:29:48 +0000

    Deploying to gh-pages from  @ d2ca2d41c97fe87139fcc7a3ea546a645b17e44a 🚀

---
+++ precache-manifest.468b9c4517cc7ff9392215016563aca4.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e39d0e2173d3ba5a38f3bbf51992180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a0d66a2a047bd717130",
+    "url": "/static/css/3.25727338.chunk.css"
+  },
+  {
+    "revision": "a8f9317dade00d8db4cf",
+    "url": "/static/css/4.a8329f31.chunk.css"
+  },
+  {
+    "revision": "06e02202326a79345bdf",
+    "url": "/static/css/main.58256005.chunk.css"
+  },
+  {
+    "revision": "5e88e9f0754d730c25d2",
+    "url": "/static/js/2.c02134d7.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.c02134d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0d66a2a047bd717130",
+    "url": "/static/js/3.ef944b89.chunk.js"
+  },
+  {
+    "revision": "a8f9317dade00d8db4cf",
+    "url": "/static/js/4.324c7d42.chunk.js"
+  },
+  {
+    "revision": "06e02202326a79345bdf",
+    "url": "/static/js/main.c4d455de.chunk.js"
+  },
+  {
+    "revision": "1d0832833f7c930af79c",
+    "url": "/static/js/runtime-main.dbdb0b9d.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a7737aa7b8221501d4ecabcbcd87ff4a",
+    "url": "/static/media/airbnb.a7737aa7.png"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "b0e41ff56d541a96f468f65dbcd72738",
+    "url": "/static/media/amazon.b0e41ff5.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "24c98066fa3cfd3c969ecfc03285fa7f",
+    "url": "/static/media/covid19_tracker.24c98066.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "81b3ee23c71ff58904d19cf26c64da0c",
+    "url": "/static/media/gmail.81b3ee23.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "272311930a46117d75718306cb794e77",
+    "url": "/static/media/linkedIn.27231193.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "c545bb42cfe44d314ac0fff66c7ff759",
+    "url": "/static/media/netlfix.c545bb42.png"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "40d5722eb1b5a5741baefdd1c292efaa",
+    "url": "/static/media/wolzon.40d5722e.png"
+  }
+]);
